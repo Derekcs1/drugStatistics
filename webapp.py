207c7d4deb83dc8fb6e71cc6445b5ba174e7ac4a
Derekcs1 <31251508+Derekcs1@users.noreply.github.com>
2017-11-30 09:58:37 -0800

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -6,28 +6,28 @@ app = Flask(__name__) #__name__ = "__main__" if this is the file that was run.  
 
 @app.route("/")
 def render_main():
-    with open('static/county_demographics.json') as demographics_data:
-        counties = json.load(demographics_data)
+    with open('static/drugs.json') as drug_data:
+        Rates = json.load(drug_data)
     if 'State' in request.args:
         selected_state = request.args["State"]
-        return render_template('home.html', response_options = get_state_options(counties), percentOver65 = percentOver65(counties, selected_state), response_state = selected_state)
-    return render_template('home.html', response_options = get_state_options(counties))
+        return render_template('home.html', response_options = get_state_options(Rates), marijuanaUse = marijuanaUse(Rates, selected_state), response_state = selected_state)
+    return render_template('home.html', response_options = get_state_options(Rates))
 
-def get_state_options(counties):
+def get_state_options(Rates):
     states = []
     options = ""
-    for c in counties:
+    for c in Rates:
         if c["State"] not in states:
             states.append(c["State"])
             options += Markup("<option value=\"" + c["State"] + "\">" + c["State"] + "</option>")
     return options
 
-def percentOver65(counties, selected_state):
-    percentOver65 = 0
+def marijuanaUse(rates, selected_state):
+    marijuanaUse = 0
     for c in counties:
         if c["State"] == selected_state:
-            percentOver65 = c["Age"]["Percent 65 and Older"]
-    return str(percentOver65)
+            marijuanaUse = c["Marijuana"]["18-25"]
+    return str(marijuanaUse)
 
 if __name__=="__main__":
     app.run(debug=False, port=54321)
