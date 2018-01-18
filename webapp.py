723a9fefc595b07ebf7da9b6435c8e6409429dee
Derekcs1 <31251508+Derekcs1@users.noreply.github.com>
2018-01-18 10:30:45 -0800

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -6,36 +6,28 @@ app = Flask(__name__) #__name__ = "__main__" if this is the file that was run.  
 
 @app.route("/")
 def render_main():
-    with open('static/drugs.json') as drug_data:
-        Rates = json.load(drug_data)
+    with open('drugs.json') as drug_data:
+        rates = json.load(drug_data)
     if 'State' in request.args:
         selected_state = request.args["State"]
-        return render_template('home.html', response_options = get_state_options(Rates), marijuanaUse = marijuanaUse(Rates, selected_state), response_state = selected_state)
-    return render_template('home.html', response_options = get_state_options(Rates))
+        return render_template('statsPage1.html', response_options = get_state_options(rates), percentAbuse = percentAbuse(rates, selected_state), response_state = selected_state)
+    return render_template('statsPage1.html', response_options = get_state_options(rates))
 
-@app.route("/marijuanausage")
-def render_marijuana_usage():
-    with open('static/drugs.json') as drug_data:
-        data = json.load(drug_data)
-    if 'State' in request.args:
-        return render_template('marijuanausage.html', response_options = get_state_options(dict), response_state = request.args['State'], marijuana_use = marijuanaUse(dict, request.args['State']))
-    return render_template('marijuanausage.html', response_options = get_state_options(data))
-
-def get_state_options(dict):
+def get_state_options(rates):
     states = []
     options = ""
-    for c in dict:
+    for c in rates:
         if c["State"] not in states:
             states.append(c["State"])
             options += Markup("<option value=\"" + c["State"] + "\">" + c["State"] + "</option>")
     return options
 
-def marijuanaUse(dict, selected_state):
-    marijuanaUse = 0
-    for c in dict:
+def percentAbuse(rates, selected_state):
+    percentAbuse = 0
+    for c in rates:
         if c["State"] == selected_state:
-            marijuanaUse = c["Marijuana"]["18-25"]
-    return str(marijuanaUse)
+            percentAbuse = c["Rates"]["Pain Relievers Abuse Past Year"]["18-25"]
+    return str(percentAbuse)
 
 if __name__=="__main__":
-    app.run(debug=False, port=54321)
+    app.run(debug=True)
